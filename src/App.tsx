bb407ee011fe5dc673bad83190ae70bca2158668
Ali_Mohammad_Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2025-04-25 17:00:56 +0330

    feat: Add main page

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Input from "./components/Input_component/input";
+
+
+export default function App() {
+  return (
+    <div className="bg-[#faf3ea] min-h-dvh w-screen scroll-smooth overflow-x-hidden select-none appearance-none font-yekanBakh flex-center">
+      {/* Input component */}
+      <Input />
+    </div>
+  )
+}
