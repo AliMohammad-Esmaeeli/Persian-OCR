325a24bf6eea3b5e72d3905b235c468baaeda73d
Ali_Mohammad_Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2025-05-08 14:58:16 +0330

    feat: Add capacitor to project

---
+++ android/app/src/main/java/com/persian_ocr/application/MainActivity.java
@@ -0,0 +1,5 @@
+package com.persian_ocr.application;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.persian_ocr.application',
+  appName: 'Persian OCR',
+  webDir: 'dist'
+};
+
+export default config;
