ebb468cdbb28921b14c67dccdb44f0b66af428d5
Ali_Mohammad_Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2025-06-24 19:46:08 +0330

    refactor: seprate components in input component

--- src/components/Main/main.tsx
+++ src/components/Main/main.tsx
@@ -1,26 +1,66 @@
-// Importing components
 import { useState } from "react";
-import Switch from "../Button/switch";
-import Input from "../Input_components/input";
 
-// Importing Svgs
-import person_svg from "../../assets/svgs/person.svg";
-import document_svg from "../../assets/svgs/document.svg";
-import FaceDetection from "../FaceDetection/faceDetection";
+// Importing dependencies
+import useTextStore from "../../state management/store";
+import { useLanguage } from "../../locales/locales";
+
+// Importing components
+import ShareButton from "../Button/shareButton";
+
+// Importing SVGs
+import Drag_and_Drop from "../Input_components/drag&drop";
+import SummarizeButton from "../Button/SummarizeButton";
+import TranslateButton from "../Button/TranslateButton";
 
 export default function Main() {
-  const [_switch, _setSwitch] = useState("Mode_1");
+  const { translations } = useLanguage();
+  const [inputImage, setInputImage] = useState<string | null>();
+  const [page, setPage] = useState("title");
+  const [loading, setLoading] = useState(false);
 
+  const { text, setText, summary, translation } = useTextStore();
   return (
     <>
-      <main className="flex-center flex-col gap-5 w-screen mt-[5vh]">
-        <Switch
-          image_1={document_svg}
-          image_2={person_svg}
-          setSwitch={_setSwitch}
+      <main className="flex-center flex-col md:flex-row gap-5 w-screen mt-[15vh]">
+        {/* Drag & Drop component */}
+        <Drag_and_Drop
+          setInputImage={setInputImage}
+          inputImage={inputImage}
+          page={page}
+          setPage={setPage}
+          setText={setText}
+          setLoading={setLoading}
+          loading={loading}
         />
 
-        {_switch === "Mode_1" ? <Input /> : <FaceDetection />}
+        {/* Display recognized text */}
+        {text && (
+          <div className="w-1/2 flex flex-col">
+            <div className="rtl flex flex-col gap-3">
+              {text && <p className="text-xl">{translations.text}</p>}
+              <p className="text-base leading-8"> {text} </p>
+
+              {/* Displaying the summary and translation */}
+
+              {summary && <p className="text-xl">{translations.summarized}</p>}
+              {summary && <p className="text-base leading-8">{summary}</p>}
+
+              {translation && (
+                <p className="text-xl">{translations.translated}</p>
+              )}
+              {translation && (
+                <p className="text-base leading-8">{translation}</p>
+              )}
+            </div>
+
+            {/* Buttons for share, translate & summarize text  */}
+            <div className="flex-center flex-col md:flex-row gap-0 md:gap-3">
+              <ShareButton Text={text} />
+              <SummarizeButton />
+              <TranslateButton />
+            </div>
+          </div>
+        )}
       </main>
     </>
   );
