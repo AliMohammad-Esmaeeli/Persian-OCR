61c26ab64bbd8ff5c7cdcc850a4e22c4e2623f26
AliMohammad-Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2025-06-28 20:13:50 +0330

    refactor: Add cookie for limitation

--- src/components/Button/SummarizeButton.tsx
+++ src/components/Button/SummarizeButton.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import axios from "axios";
-import { Button } from "@heroui/react";
+import { addToast, Button } from "@heroui/react";
 import Loading from "../Loading/loading";
 import { useLanguage } from "../../locales/locales";
 import useTextStore from "../../state management/store";
 import GeminiSvg from "../../assets/svgs/gemini.svg";
+import { GetCookie, AddCookie } from "../Cookie/cookie";
 
 export default function SummarizeButton() {
   const { translations } = useLanguage();
@@ -14,60 +15,87 @@ export default function SummarizeButton() {
   const [error, setError] = useState("");
 
   const handleSummarize = async () => {
-    const BACKEND_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${
-      import.meta.env.GEMINI_API_KEY
-    }`;
-    if (!text.trim()) {
-      setError("Please enter some text to summarize.");
-      return;
-    }
-    setLoading(true);
-    setError("");
-    setSummary("");
-    try {
-      const response = await axios.post(BACKEND_URL, {
-        contents: [
-          {
-            parts: [
-              {
-                text: `Summarize the following text in persian: ${text}`,
-              },
-            ],
-          },
-        ],
-        generationConfig: {
-          temperature: 0.7,
-          maxOutputTokens: 200,
+    if (GetCookie("Useage") === "3") {
+      addToast({
+        title: translations.warningLimitTitle,
+        description: translations.warningLimitDescription,
+        color: "warning",
+        classNames: {
+          title: "font-yekanBakh !text-sm !text-red-500",
+          description: "font-yekanBakh !text-xs !text-red-400",
         },
       });
-      if (
-        response.data &&
-        response.data.candidates &&
-        response.data.candidates.length > 0 &&
-        response.data.candidates[0].content &&
-        response.data.candidates[0].content.parts &&
-        response.data.candidates[0].content.parts.length > 0 &&
-        response.data.candidates[0].content.parts[0].text
-      ) {
-        const actualSummary = response.data.candidates[0].content.parts[0].text;
-        setSummary(actualSummary);
-      } else {
-        setError(
-          "Could not extract summary from Gemini API response. Response structure might be unexpected."
-        );
-        console.error(
-          "Unexpected Gemini API response structure:",
-          response.data
-        );
+    } else {
+      const BACKEND_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${
+        import.meta.env.GEMINI_API_KEY
+      }`;
+      setLoading(true);
+      setError("");
+      setSummary("");
+      try {
+        const response = await axios.post(BACKEND_URL, {
+          contents: [
+            {
+              parts: [
+                {
+                  text: `Summarize the following text in persian: ${text}`,
+                },
+              ],
+            },
+          ],
+          generationConfig: {
+            temperature: 0.7,
+            maxOutputTokens: 200,
+          },
+        });
+        if (
+          response.data &&
+          response.data.candidates &&
+          response.data.candidates.length > 0 &&
+          response.data.candidates[0].content &&
+          response.data.candidates[0].content.parts &&
+          response.data.candidates[0].content.parts.length > 0 &&
+          response.data.candidates[0].content.parts[0].text
+        ) {
+          const actualSummary =
+            response.data.candidates[0].content.parts[0].text;
+          setSummary(actualSummary);
+          switch (GetCookie("Useage")) {
+            case "0":
+            case null:
+            case undefined:
+            default:
+              AddCookie("Useage", "1");
+              break;
+            case "1":
+              AddCookie("Useage", "2");
+              break;
+            case "2":
+              AddCookie("Useage", "3");
+              break;
+          }
+        } else {
+          setError(
+            "Could not extract summary from Gemini API response. Response structure might be unexpected."
+          );
+          console.error(
+            "Unexpected Gemini API response structure:",
+            response.data
+          );
+        }
+      } catch (error: any) {
+        console.error("Error sending text to backend:", error);
+        setError(error);
+        if (
+          error.response &&
+          error.response.data &&
+          error.response.data.error
+        ) {
+          setError(error.response.data.error);
+        }
+      } finally {
+        setLoading(false);
       }
-    } catch (error: any) {
-      console.error("Error sending text to backend:", error);
-      setError(error);
-      if (error.response && error.response.data && error.response.data.error) {
-        setError(error.response.data.error);
-      }
-    } finally {
-      setLoading(false);
     }
   };
   return (
